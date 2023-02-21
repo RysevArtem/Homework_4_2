785f160c95f9eca8a54ee5918ff76c932b1f3361
Vincento1986 <vincentoselgari@gmail.com>
2023-02-21 22:11:56 +0300

    Cleanup code

--- test_search.py
+++ test_search.py
@@ -1,9 +1,12 @@
 from selene import be, have
 from selene.support.shared import browser
 
+
 def test_search(browser_max, clear_field):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
 def test_search_negative(browser_max, clear_field):
     browser.element('[name="q"]').type('gjklsfdhgjksdghdskj').press_enter()
     browser.element('.card-section').should(have.text('По запросу gjklsfdhgjksdghdskj ничего не найдено. '))
