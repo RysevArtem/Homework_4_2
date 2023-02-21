5346af1fa39fc87354cca7a7627be986e756414c
Vincento1986 <vincentoselgari@gmail.com>
2023-02-21 22:05:08 +0300

    Исправления негативного теста

--- test_search.py
+++ test_search.py
@@ -5,5 +5,5 @@ def test_search(browser_max, clear_field):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 def test_search_negative(browser_max, clear_field):
-    browser.element('[name="q"]').type('Стихи Лермонтова').press_enter()
-    browser.element('[id="search"]').should(have.text('Три девицы под окном пряли поздно вечерком'))
+    browser.element('[name="q"]').type('gjklsfdhgjksdghdskj').press_enter()
+    browser.element('.card-section').should(have.text('По запросу gjklsfdhgjksdghdskj ничего не найдено. '))
